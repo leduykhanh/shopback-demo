6ad06ec8b8153e1720be109cf8ce551e65307cd1
Le Duy Khanh <ledu0003@e.ntu.edu.sg>
2018-05-16 04:13:51 +0800

    clean up

--- app/components/Carousel/index.js
+++ app/components/Carousel/index.js
@@ -6,6 +6,7 @@
 
 import React, { Children, Component } from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import './scss/carousel.scss';
 
 function getPosition(event) {
@@ -19,120 +20,111 @@ function getPosition(event) {
 }
 
 function CSSTranslate(position, axis) {
+  const positionCss = (axis === 'horizontal') ? [position, 0, 0] : [0, position, 0];
+  const transitionProp = 'translate3d';
 
-    const positionCss = (axis === 'horizontal') ? [position, 0, 0] : [0, position, 0];
-    const transitionProp = 'translate3d';
-
-    const translatedPosition = '(' + positionCss.join(',') + ')';
-    console.log("CSSTranslate", position)
-    // console.log("CSSTranslate", transitionProp + translatedPosition)
-    return transitionProp + translatedPosition;
-};
+  const translatedPosition = `(${positionCss.join(',')})`;
+  return transitionProp + translatedPosition;
+}
 
 class Carousel extends Component { // eslint-disable-line react/prefer-stateless-function
 
   static defaultProps = {
-    centerMode: false,
-    centerSlidePercentage: 100
+    tolerance: 100,
   };
 
   constructor(props) {
     super(props);
-    this.tolerance = 100;
-    let { children } = this.props;
-    let childrenCount = Children.count(children);
+    this.tolerance = props.tolerance;
+    const { children } = this.props;
+    const childrenCount = Children.count(children);
     this.childrenCount = childrenCount;
     this.state = {
       selectedItem: childrenCount,
       items: [].concat(children).concat(children).concat(children),
     };
 
-    this._handleSwipeStart = this._handleSwipeStart.bind(this);
-    this._handleSwipeMove = this._handleSwipeMove.bind(this);
-    this._handleSwipeEnd = this._handleSwipeEnd.bind(this);
+    this.handleSwipeStart = this.handleSwipeStart.bind(this);
+    this.handleSwipeMove = this.handleSwipeMove.bind(this);
+    this.handleSwipeEnd = this.handleSwipeEnd.bind(this);
 
-    this._onMouseDown = this._onMouseDown.bind(this);
-    this._onMouseMove = this._onMouseMove.bind(this);
-    this._onMouseUp = this._onMouseUp.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
   }
 
   componentDidMount() {
     this.setState({
-      itemSize : this.itemsRef[0].clientWidth,
+      itemSize: this.itemsRef[0].clientWidth,
     });
   }
 
   setItemsRef(node, index) {
-        if (!this.itemsRef) {
-            this.itemsRef = [];
-        }
-        this.itemsRef[index] = node;
+    if (!this.itemsRef) {
+      this.itemsRef = [];
     }
-
-  resetItems(position) {
-
+    this.itemsRef[index] = node;
   }
 
   moveTo(position) {
-        console.log("position", position);
-        console.log("this.mouseDown ", this.mouseDown );
-        const firstPosition = this.childrenCount;
-        const lastPosition = 2 * this.childrenCount - 1;
+    const firstPosition = this.childrenCount;
+    const lastPosition = 2 * this.childrenCount - 1;
+    let newPosition = position;
 
-        if (position < firstPosition ) {
-          position = lastPosition;
-        }
-
-        if (position > lastPosition) {
-          position = firstPosition;
-        }
-
-        this.setState({
-            selectedItem: position
-        });
+    if (position < firstPosition) {
+      newPosition = lastPosition;
     }
 
-  onSwipeLeft(position, event) {
-    this.moveTo(this.state.selectedItem + (typeof position === 'Number' ? positions : 1));
+    if (position > lastPosition) {
+      newPosition = firstPosition;
+    }
+
+    this.setState({
+      selectedItem: newPosition,
+    });
   }
 
-  onSwipeRight(position, event) {
-    this.moveTo(this.state.selectedItem - (typeof position === 'Number' ? positions : 1));
+  onSwipeLeft(position) {
+    this.moveTo(this.state.selectedItem + (typeof position === 'number' ? position : 1));
   }
 
-  _onMouseDown(event) {
+  onSwipeRight(position) {
+    this.moveTo(this.state.selectedItem - (typeof position === 'number' ? position : 1));
+  }
+
+  onMouseDown(event) {
     this.mouseDown = true;
 
-    document.addEventListener('mouseup', this._onMouseUp);
-    document.addEventListener('mousemove', this._onMouseMove);
+    document.addEventListener('mouseup', this.onMouseUp);
+    document.addEventListener('mousemove', this.onMouseMove);
 
-    this._handleSwipeStart(event);
+    this.handleSwipeStart(event);
   }
 
-  _onMouseMove(event) {
+  onMouseMove(event) {
     if (!this.mouseDown) {
       return;
     }
 
-    this._handleSwipeMove(event);
+    this.handleSwipeMove(event);
   }
 
-  _onMouseUp(event) {
+  onMouseUp(event) {
     this.mouseDown = false;
 
-    document.removeEventListener('mouseup', this._onMouseUp);
-    document.removeEventListener('mousemove', this._onMouseMove);
+    document.removeEventListener('mouseup', this.onMouseUp);
+    document.removeEventListener('mousemove', this.onMouseMove);
 
-    this._handleSwipeEnd(event);
+    this.handleSwipeEnd(event);
   }
 
-  _handleSwipeStart(event) {
+  handleSwipeStart(event) {
     const { x, y } = getPosition(event);
     this.moveStart = { x, y };
     this.onSwipeStart(event);
   }
 
-  _handleSwipeMove(event) {
+  handleSwipeMove(event) {
     if (!this.moveStart) {
       return;
     }
@@ -149,8 +141,7 @@ class Carousel extends Component { // eslint-disable-line react/prefer-stateless
     this.deltaX = deltaX;
   }
 
-  _handleSwipeEnd(event) {
-
+  handleSwipeEnd(event) {
     const { tolerance } = this;
     if (this.moving) {
       if (this.deltaX < -tolerance) {
@@ -166,81 +157,77 @@ class Carousel extends Component { // eslint-disable-line react/prefer-stateless
     this.movePosition = null;
   }
 
-    onSwipeStart() {
-        this.setState({
-            swiping: true
-        });
+  onSwipeStart() {
+    this.setState({
+      swiping: true,
+    });
+  }
+
+  onSwipeEnd() {
+    this.resetPosition();
+    this.setState({
+      swiping: false,
+    });
+  }
+
+  onSwipeMove(deltaX) {
+    const initialBoundry = 0;
+    const currentPosition = this.getPosition(this.state.selectedItem);
+    const finalBoundry = this.getPosition(Children.count(this.props.children) - 1);
+
+    let handledDelta = deltaX;
+
+    if (currentPosition === initialBoundry && deltaX > 0) {
+      handledDelta = 0;
     }
 
-    onSwipeEnd() {
-        this.resetPosition();
-        this.setState({
-            swiping: false
-        });
-        ;
+    if (currentPosition === finalBoundry && deltaX < 0) {
+      handledDelta = 0;
     }
 
-    onSwipeMove(deltaX) {
+    const position = currentPosition + (100 / (this.state.itemSize / handledDelta)) + '%';
 
-        const initialBoundry = 0;
+    this.setPosition(position);
 
-        const currentPosition = this.getPosition(this.state.selectedItem);
-        console.log("currentPosition", currentPosition);
-        const finalBoundry = this.getPosition(Children.count(this.props.children) - 1);
+    const hasMoved = Math.abs(deltaX) > this.tolerance;
 
-        let handledDelta = deltaX;
-
-        if (currentPosition === initialBoundry && deltaX > 0) {
-            handledDelta = 0;
-        }
-
-        if (currentPosition === finalBoundry && deltaX < 0) {
-            handledDelta = 0;
-        }
-
-        const position = currentPosition + (100 / (this.state.itemSize / handledDelta)) + '%';
-
-        this.setPosition(position);
-
-        const hasMoved = Math.abs(deltaX) > this.tolerance;
-
-        return hasMoved;
-    }
+    return hasMoved;
+  }
 
 
-    getPosition(index) {
-        return - index * 100;
-    }
+  getPosition(index) {
+    return - index * 100;
+  }
 
-    resetPosition = () => {
-        const currentPosition = this.getPosition(this.state.selectedItem) + '%';
-        this.setPosition(currentPosition);
-    }
+  resetPosition() {
+    const currentPosition = this.getPosition(this.state.selectedItem) + '%';
+    this.setPosition(currentPosition);
+  }
 
-    setPosition = (position) => {
-        const list = ReactDOM.findDOMNode(this.listRef);
-        [
-            'WebkitTransform',
-            'MozTransform',
-            'MsTransform',
-            'OTransform',
-            'transform',
-            'msTransform'
-        ].forEach((prop) => {
-            list.style[prop] = CSSTranslate(position, 'horizontal');
-        });
-    }
+  setPosition(position) {
+    const list = ReactDOM.findDOMNode(this.listRef);
+    [
+      'WebkitTransform',
+      'MozTransform',
+      'MsTransform',
+      'OTransform',
+      'transform',
+      'msTransform',
+    ].forEach((prop) => {
+      list.style[prop] = CSSTranslate(position, 'horizontal');
+    });
+  }
 
   renderItems() {
     return Children.map(this.state.items, (item, index) => {
       const slideProps = {
-                ref: (e) => this.setItemsRef(e, index),
-                key: 'itemKey' + index,
-                className: `slide ${index === this.state.selectedItem ? 'selected' : ''}`,
+              ref: (e) => this.setItemsRef(e, index),
+              key: 'itemKey' + index,
+              className: `slide ${index === this.state.selectedItem ? 'selected' : ''}`,
             };
       return (
                 <li {...slideProps}>
-                    { item }
+                  { item }
                 </li>
             );
     });
@@ -252,10 +239,10 @@ class Carousel extends Component { // eslint-disable-line react/prefer-stateless
         <div className="slider-wrapper axis-horizontal">
           <ul
             className="slider"
-            onMouseDown={ this._onMouseDown }
-            onTouchStart={ this._handleSwipeStart }
-            onTouchEnd={ this._handleSwipeEnd }
-            ref={(node) => { this.listRef = node} }>
+            onMouseDown={this.onMouseDown}
+            onTouchStart={this.handleSwipeStart}
+            onTouchEnd={this.handleSwipeEnd}
+            ref={(node) => { this.listRef = node; }}>
             { this.renderItems() }
           </ul>
         </div>
@@ -265,7 +252,8 @@ class Carousel extends Component { // eslint-disable-line react/prefer-stateless
 }
 
 Carousel.propTypes = {
-
+  children: PropTypes.node,
+  tolerance: PropTypes.number,
 };
 
 export default Carousel;
